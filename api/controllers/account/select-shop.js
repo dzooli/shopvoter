af31d740b63dc36fc36286b108d3661c70b3d0bf
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-09-30 03:21:22 +0200

    syntax fix

--- api/controllers/account/select-shop.js
+++ api/controllers/account/select-shop.js
@@ -1,39 +1,30 @@
 module.exports = {
+  friendlyName: "Select shop",
 
-
-  friendlyName: 'Select shop',
-
-
-  description: 'Select the actual shop for the user.',
+  description: "Select the actual shop for the user.",
   extendedDescription: `When the user is logged in first time he/she have to select the current shop. The actual user is
   only able to change tha shop right after login.`,
 
-
   inputs: {
     shop_id: {
-      type: 'number',
-      example: '1',
+      type: "number",
+      example: "1",
       description: "The shop id",
-      whereToGet: { description: "Select the shop after login with the shown buttons."},
-    }
-
+      whereToGet: {
+        description: "Select the shop after login with the shown buttons.",
+      },
+    },
   },
 
   exits: {
     redirect: {
-      description: "Redirect to the login page if not logged in."
-      responseType: "redirect"
+      description: "Redirect to the login page if not logged in.",
+      responseType: "redirect",
     },
-
   },
 
-
   fn: async function (inputs) {
-
     // All done.
     return;
-
-  }
-
-
+  },
 };
