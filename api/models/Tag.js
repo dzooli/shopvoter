0e0b867e984f9be70c23c225ac435c47dd3ca608
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-09-30 02:56:11 +0200

    Tag model added

---
+++ api/models/Tag.js
@@ -0,0 +1,46 @@
+/**
+ * Tag.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    shop_id: {
+      model: "shop",
+      required: true,
+    },
+    user_id: {
+      model: "user",
+      required: true,
+    },
+    tagValue: {
+      type: "number",
+      columnType: "int",
+      isInteger: true,
+      required: true,
+    },
+    createdAt: {
+      type: "number",
+      columnType: "bigint",
+      isInteger: true,
+    },
+    updatedAt: {
+      type: "number",
+      columnType: "bigint",
+      isInteger: true,
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
