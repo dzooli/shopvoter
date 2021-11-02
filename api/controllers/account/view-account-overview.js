7b1f31c6d3c424a4290dbc205ed28fdf649a3a45
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-11-02 02:03:05 +0100

    Added navigation and access control for Admin/Users data-table "Edit" action.

--- api/controllers/account/view-account-overview.js
+++ api/controllers/account/view-account-overview.js
@@ -3,15 +3,52 @@ module.exports = {
 
   description: 'Display "Account Overview" page.',
 
+  inputs: {
+    id: {
+      type: "number",
+      required: false,
+    },
+  },
+
   exits: {
     success: {
       viewTemplatePath: "pages/account/account-overview",
     },
+    notfound: {
+      description: "User not found",
+      responseType: "notfound",
+    },
+    unauthorized: {
+      description: "Not authorized action",
+      responseType: "unauthorized"
+    }
   },
 
-  fn: async function () {
-    var company = await Company.findOne({
-      id: this.req.me.company_id
+  fn: async function (inputs) {
+    let req = this.req;
+    let res = this.res;
+    let user = req.me;
+    
+    if (typeof inputs !== undefined && !_.isUndefined(inputs.id)) {
+      sails.log.debug("Typeof inputs.id: ", typeof inputs);
+      user = await User.findOne({id: inputs.id});
+    }
+    if (!user) {
+      throw "notfound";
+    }
+
+    let companyId = user.company_id;
+
+    var userIsSuper = await sails.helpers.hasRole(req.me.id, "superuser");
+    var userIsCa=await sails.helpers.hasRole(req.me.id, "companyadmin");
+
+    // admins can view all user profiles
+    if (userIsSuper 
+    // companyadmins can view user profiles from the same company
+    || (userIsCa && req.me.company_id == companyId)
+    ) {
+    let company = await Company.findOne({
+      id: companyId,
     });
 
     // If billing features are enabled, include our configured Stripe.js
@@ -22,5 +59,8 @@ module.exports = {
         undefined,
       companyName: company ? company.name : "unknown",
     };
+  } else {
+    throw 'unauthorized';
+  }
   },
 };
