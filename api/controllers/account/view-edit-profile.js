40a4cf5e8704d69a78fdc3036b2de14bea8e090a
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-11-23 03:09:21 +0100

    assigned roles filtered out

--- api/controllers/account/view-edit-profile.js
+++ api/controllers/account/view-edit-profile.js
@@ -58,8 +58,9 @@ module.exports={
     }
 
     // Prepare the assigned roles return value
-    var assignedRoles = [];
-    var assignedRoleLevels = [];
+    var assignedRoles=[];
+    var assignedRoleLevels=[];
+    var assignedRoleIds=[];
     var userRoles=await Userrole.find({
       where: { user_id: id },
     }).populate("role_id").omit([
@@ -67,31 +68,47 @@ module.exports={
       "updatedAt",
       "user_id",
     ]);
-    userRoles.forEach(function (element, idx) {
+    userRoles.forEach(function (element, idx)
+    {
       assignedRoles.push({
         id: element.role_id.id,
         name: element.role_id.name,
         level: element.role_id.level
       });
       assignedRoleLevels.push(element.role_id.level);
+      assignedRoleIds.push({id: element.role_id.id});
     });
 
     // Find the available roles with greater or equal level as the highest (lowest level = highest priority) assigned
-    var availableRoles = [];
+    var availableRoles=[];
     var userLevel=maxLevel;
     if (assignedRoles)
     {
       userLevel=Math.min(...assignedRoleLevels);
     }
+    var unassignedRoles=[];
     availableRoles=await Role.find({ level: { '>=': userLevel } }).select([
       "id",
       "name",
       "level"
     ]);
+    availableRoles.forEach(function (element, idx)
+    {
+      // Do not add the already assigned roles to the unassigned list
+      if (!_.find(assignedRoleIds, { id: element.id }))
+      {
+        unassignedRoles.push({
+          id: element.id,
+          name: element.name,
+          level: element.level,
+        });
+      }
+    });
+    unassignedRoles=_.difference(unassignedRoles, assignedRoles);
 
     return {
       user: user,
-      rolesAvailable: availableRoles,
+      rolesAvailable: unassignedRoles,
       rolesAssigned: assignedRoles
     };
 
